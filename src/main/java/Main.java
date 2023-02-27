8dee6ef28367862990cc970447171ce7d95b8cf1
Верхозина Екатерина <April.Kitty@yandex.ru>
2023-02-27 23:06:09 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+        Cat obj = MyMock.mock(new Cat(Cat.Trait.kind), Cat.class);
+        String response = obj.PetTheCat();
+        System.out.println(response); //null
+
+        MyMock.setReturn(obj.PetTheCat(), "meow");
+        response = obj.PetTheCat();
+        System.out.println(response); //meow
+
+        MyMock.setReturn(obj.PetTheCat(), "rrr");
+        response = obj.PetTheCat();
+        System.out.println(response);
+
+        response = Cat.GiveBox(Cat.Box.big);
+        System.out.println(response);
+        MyMock.setReturn(Cat.GiveBox(Cat.Box.big), "meow");
+        response = Cat.GiveBox(Cat.Box.big);
+        System.out.println(response);
+    }
+}
---
+++ src/main/java/MyMock.java
@@ -0,0 +1,77 @@
+import org.springframework.cglib.proxy.*;
+
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MyMock {
+    //mock, @Mock, when, thenReturn, thenThrow
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    public @interface MockField {
+
+    }
+
+    private static class CallInfo<T> {
+        private String className;
+        private String methodName;
+        private Object[] arguments;
+        private T result;
+
+        public CallInfo(String className, String methodName, Object[]arguments) {
+            this.className = className;
+            this.methodName = methodName;
+            this.arguments = arguments;
+        }
+
+        public void setResult(T t) {this.result = t;}
+
+        public T getResult() {return result;}
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof CallInfo)) return false;
+            CallInfo<?> callInfo = (CallInfo<?>) o;
+            return Objects.equals(className, callInfo.className) && Objects.equals(methodName, callInfo.methodName) && Arrays.equals(arguments, callInfo.arguments);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(className, methodName);
+            result = 31 * result + Arrays.hashCode(arguments);
+            return result;
+        }
+    }
+
+    private static ArrayList<CallInfo> calls = new ArrayList();
+    private static ArrayList<CallInfo> setReturns = new ArrayList();
+
+    public static void setReturn(Object oldR, Object newR) {
+        CallInfo call = calls.get(calls.size() - 1);
+        call.setResult(newR);
+        setReturns.add(call);
+    }
+
+    private static class MyMethodWrapper<T> implements MethodInterceptor{
+        T orig;
+        MyMethodWrapper(T orig){ this.orig = orig;}
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args,
+                                MethodProxy proxy) throws Throwable
+        {
+            calls.add(new CallInfo(obj.getClass().getName(), method.getName(), args));
+            if (setReturns.contains(new CallInfo(obj.getClass().getName(), method.getName(), args)))
+                return setReturns.get(
+                        setReturns.lastIndexOf(new CallInfo(obj.getClass().getName(), method.getName(), args))
+                ).getResult();
+            return null;
+        }
+    }
+
+    public static <T> T mock(T obj, Class<T> aClass) {
+        return (T) Enhancer.create(aClass, new MyMethodWrapper<>(obj));
+    }
+}
